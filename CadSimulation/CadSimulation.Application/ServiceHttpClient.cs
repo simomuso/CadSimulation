608ada039946ae0ca030194be1a8c6a31625ae5a
Simone Musetti <simone.musetti@outlook.com>
2024-11-27 20:55:23 +0100

    Add persistence service http client

---
+++ CadSimulation/CadSimulation.Application/ServiceHttpClient.cs
@@ -0,0 +1,30 @@
+﻿namespace CadSimulation.Application
+{
+    public interface IServiceHttpClient
+    {
+        Task<string> GetAsync();
+        Task SaveAsync(string content);
+    }
+
+    public class ServiceHttpClient : IServiceHttpClient
+    {
+        private readonly Uri _serviceUri;
+        private readonly HttpClient _httpClient = new();
+
+        public ServiceHttpClient(Uri serviceUri)
+        {
+            _serviceUri = serviceUri;
+        }
+
+        public async Task<string> GetAsync()
+        {
+            var response = await _httpClient.GetAsync(_serviceUri);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task SaveAsync(string content)
+        {
+            await _httpClient.PostAsync(_serviceUri, new StringContent(content));
+        }
+    }
+}
